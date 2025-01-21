c92600f6c6cba548407edf0bee4896ceb4bfc287
Kadiga6 <9510@holbertonstudents.com>
2025-01-21 12:05:03 +0100

    1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < my_list:
-        return(None)
-    if idx > my_list:
-e       reburn(None)
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
+

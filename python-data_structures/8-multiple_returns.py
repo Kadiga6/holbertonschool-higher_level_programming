e4e686283f3a7a09fa72569ebfe7c562b5b14094
Kadiga6 <9510@holbertonstudents.com>
2025-01-23 14:36:12 +0100

    8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if !sentence:
-        tuple[0] == None
-    return len(sentence)
-    if sentence != 0:
-        print("{:0}".format(sentence))
-    return  
-        
+    if not sentence:
+        return (0, None)
+    return (len(sentence), sentence[0])

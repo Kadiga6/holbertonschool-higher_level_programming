3b995815ef6549f9e81dc82ede9fa773f4cf2053
Kadiga6 <9510@holbertonstudents.com>
2025-01-23 14:29:09 +0100

    7

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if !sentence:
+        tuple[0] == None
+    return len(sentence)
+    if sentence != 0:
+        print("{:0}".format(sentence))
+    return  
+        

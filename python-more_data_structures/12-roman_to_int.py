38c90a6bfb25890925e167873313bb831f6206b3
Kadiga6 <9510@holbertonstudents.com>
2025-01-26 01:06:32 +0100

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-
+    to_arabe = {'I': 1, 'V': 5, 'X': 10,
+                'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    roman_values = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
-
-    for i in range(len(roman_string)):
-        # Si ce n'est pas le dernier caractère et
-        # la valeur actuelle est inférieure à la suivante
-        if i < len(roman_string) - 1 and roman_values[roman_string[i]] < roman_values[roman_string[i + 1]]:
-            total -= roman_values[roman_string[i]]
+    prev = 0
+    for letter in reversed(roman_string):
+        value = to_arabe[letter]
+        if value < prev:
+            total -= value
         else:
-            total += roman_values[roman_string[i]]
-
+            total += value
+        prev = value
     return total

44fcd701aaf7157b1667c51af9d3748ecf54239a
Kadiga6 <9510@holbertonstudents.com>
2025-01-28 18:44:09 +0100

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """Définit un carré."""
+    def __init__(self, size):
+        """Initialise le carré avec un attribut privé size."""
+        self.__size = size

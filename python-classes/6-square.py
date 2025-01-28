91165ae35c15a5e55b3379d4e6e03e9332ce8d6f
Kadiga6 <9510@holbertonstudents.com>
2025-01-28 21:55:49 +0100

    6

---
+++ python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(n, int) and n >= 0 for n in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+
+        # Gérer les lignes initiales dues à position[1]
+        print("\n" * self.__position[1], end="")
+
+        # Gérer chaque ligne du carré
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)

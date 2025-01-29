5584dc4e9c818a53e93db61a38bf87832a50dac8
Kadiga6 <9510@holbertonstudents.com>
2025-01-29 15:46:15 +0100

    3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+
 class Square:
+    """ Classe qui définit un carré. """
+
     def __init__(self, size=0):
+        """ Initialisation du carré avec une taille optionnelle. """
+
         if not isinstance(size, int):
+            """ Vérifie si size est un entier """
             raise TypeError("size must be an integer")
         if size < 0:
+            """ Vérifie si size est négatif """
             raise ValueError("size must be >= 0")
-        self.__size = size  # L'attribut de l'instance doit être initialisé
+        self.__size = size
+        """ L'attribut de l'instance doit être initialisé """
 
     def area(self):
-        return self.__size * self.__size  # Retourne l'aire du carré
+        """Retourne l'aire du carré"""
+        return self.__size * self.__size

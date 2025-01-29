c890a7537609803aab7b754c717a9a42bb832c8f
Kadiga6 <9510@holbertonstudents.com>
2025-01-29 15:41:58 +0100

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+""" définr une classe square """
+
+
 class Square:
+    """ Classe qui définit un carré. """
+
     def __init__(self, size=0):
-        if not isinstance(size, int):  # Vérifie si size est un entier
+
+        """ Initialisation du carré avec une taille optionnelle. """
+
+        if not isinstance(size, int):
+            """ Vérifie si size est un entier """
             raise TypeError("size must be an integer")
-        if size < 0:  # Vérifie si size est négatif
+        if size < 0:
+            """ Vérifie si size est négatif """
             raise ValueError("size must be >= 0")
-        self.__size = size  # Si tout est correct, attribue la valeur
+
+        self.__size = size
+        """ Si tout est correct, attribue la valeur """

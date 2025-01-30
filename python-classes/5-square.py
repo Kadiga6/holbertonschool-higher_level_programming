87348f44d2c66819ac2a2fd58f2119e49da4aff9
Kadiga6 <9510@holbertonstudents.com>
2025-01-30 13:55:41 +0100

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+""" définr une classe square """
+
+
 class Square:
+    """ Classe qui définit un carré. """
+
     def __init__(self, size=0):
+        """ Initialisation du carré avec une taille optionnelle. """
         self.__size = size
 
     @property
@@ -10,12 +16,15 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
+            """ Vérifie si size est un entier """
             raise TypeError("size must be an integer")
         if value < 0:
+            """ Vérifie si size est négatif """
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
+        """ retourne l'aire """
         return self.__size * self.__size
 
     def my_print(self):

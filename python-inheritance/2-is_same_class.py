d36eea33ecaf757391524b177c622fce0e5f2eb4
Kadiga6 <9510@holbertonstudents.com>
2025-02-05 21:55:07 +0100

    2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+
+    """Vérifie si l'objet est une instance exacte de la classe a_class"""
+    if type(obj) is a_class:
+        """Si l'objet est exactement de la classe a_class, retourne True"""
+        return True
+    """Sinon, retourne False"""
+    return False

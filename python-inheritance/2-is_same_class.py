c01d3dc403a879a145aee00ccc1b82de9117956d
Kadiga6 <9510@holbertonstudents.com>
2025-02-05 22:04:18 +0100

    2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Spécifie l'interpréteur à utiliser pour exécuter ce script"""
+
+"""Définition de la fonction is_same_class, qui prend deux arguments"""
 
 
 def is_same_class(obj, a_class):
-
     """Vérifie si l'objet est une instance exacte de la classe a_class"""
     if type(obj) is a_class:
         """Si l'objet est exactement de la classe a_class, retourne True"""

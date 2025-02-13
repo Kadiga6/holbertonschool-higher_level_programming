266a964bb8f3bebd126197c4c1dd2a2f501ffe1b
Kadiga6 <9510@holbertonstudents.com>
2025-02-13 15:43:26 +0100

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 
+"""
+Écrit une chaîne de caractères dans un fichier texte UTF-8 et retourne le nombre de caractères écrits.
 
+    Args:
+        filename (str): Le nom du fichier dans lequel écrire.
+        text (str): La chaîne de caractères à écrire dans le fichier.
 
+    Returns:
+        int: Le nombre de caractères écrits dans le fichier.
+"""
+
+def write_file(filename="", text=""):
+    """Utiliser l'instruction 'with' pour ouvrir le fichier"""
+    with open(filename, "w", encoding='utf-8') as file:
+        """ Écrire le texte dans le fichier """
+        num_chars = file.write(text)
+    """Retourner le nombre de caractères écrits"""
+    return num_chars

de1c8c396fcd0356f44ec9346634dc9bc782bbf0
Kadiga6 <9510@holbertonstudents.com>
2025-02-13 15:46:54 +0100

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
 """
-Écrit une chaîne de caractères dans un fichier texte UTF-8 et retourne le nombre de caractères écrits.
-
-    Args:
-        filename (str): Le nom du fichier dans lequel écrire.
-        text (str): La chaîne de caractères à écrire dans le fichier.
-
-    Returns:
-        int: Le nombre de caractères écrits dans le fichier.
+This module contains the write_file function
 """
+
 
 def write_file(filename="", text=""):
     """Utiliser l'instruction 'with' pour ouvrir le fichier"""

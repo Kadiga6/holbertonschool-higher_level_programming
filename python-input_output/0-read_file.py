cc15e46f92bdfb0c994651d16cd0524d8388a75d
Kadiga6 <9510@holbertonstudents.com>
2025-02-13 15:30:37 +0100

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,9 +5,9 @@ This module contains the read_file function
 
 
 def read_file(filename=""):
-    # Utiliser l'instruction 'with' pour ouvrir le fichier
+    """ Utiliser l'instruction 'with' pour ouvrir le fichier """
     with open(filename, "r", encoding="utf-8") as f:
-        # Lire le contenu du fichier
+        """ Lire le contenu du fichier """
         contenu = f.read()
         """end="" pour éviter une ligne vide supplémentaire"""
         print(f.read(), end="")

85e2386b957a76e49e6c872e763fdacfa409b5a6
Kadiga6 <9510@holbertonstudents.com>
2025-02-14 20:55:54 +0100

    3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,5 +5,14 @@ This module contains the JSON_file function
 
 
 def to_json_string(my_obj):
+    """
+    Retourne la représentation JSON d'un objet (chaîne de caractères).
+
+    Args:
+        my_obj (any type): L'objet à convertir en JSON.
+
+    Returns:
+        str: La représentation JSON de l'objet.
+    """
     import json
     return json.dumps(my_obj)
